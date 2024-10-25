bf81cd9553fa7449ec0ba7269d865f108fa29b9e
Yogesh Shrestha <iamyogess@gmail.com>
2024-10-25 15:41:24 +0545

    initial setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,33 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./configs/connectDB.js";
+import {
+  errorResponseHandler,
+  invalidPathHandler,
+} from "./middlewares/errorHandler.js";
+
+//.env
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+//connectDB
+connectDB();
 
-app.get("/", (req,res) => {
-    res.send("hello hi")
-})
+//middlewares
+app.use(express.json());
+app.use(cors());
 
+//invalid path handlers
+app.use(invalidPathHandler);
+app.use(errorResponseHandler);
+
+//port
+const PORT = process.env.PORT;
+
+//listening to the server
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
 });
